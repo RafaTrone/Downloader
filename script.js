311c7726d1609776dcaf7574a7d82968ba37e721
Rafael Silvestrone <rafael.silvestrone@gmail.com>
2022-01-05 13:54:44 -0300

    Iniciando estrutura da pagina de download

---
+++ script.js
@@ -0,0 +1,12 @@
+var convertBtn = document.querySelector('.convert-button');
+var URLinput = document.querySelector('.URL-input');
+
+convertBtn.addEventListener('click', () => {
+    console.log(`URL: ${URLinput.value}`);
+    sendURL(URLinput.value);
+});
+
+function sendURL(URL) {
+    //colocaremos o código aqui mais tarde
+    // We will put code here later
+}
